ba15ca559d8ad6cd46f3e5cd2371d9cb6caa33b0
Hope <hope@hopes-mac.fios-router.home>
2018-02-13 04:21:17 -0500

    Finish add GroupVC UI and UX Set up createGroupVC,

---
+++ breakpoint/Controller/CreateGroupsVC.swift
@@ -0,0 +1,50 @@
+//
+//  CreateGroupsVC.swift
+//  breakpoint
+//
+//  Created by Hope on 2/13/18.
+//  Copyright © 2018 ARC. All rights reserved.
+//
+
+import UIKit
+
+class CreateGroupsVC: UIViewController {
+
+    @IBOutlet weak var titleTextField: InsetTextField!
+    @IBOutlet weak var descriptionTextField: InsetTextField!
+    @IBOutlet weak var emailSearchTextField: InsetTextField!
+    @IBOutlet weak var groupMemberLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var doneBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func doneBtnPressed(_ sender: Any) {
+    
+    }
+    
+    
+    @IBAction func closeBtnPressed(_ sender: Any) {
+    
+    }
+}
