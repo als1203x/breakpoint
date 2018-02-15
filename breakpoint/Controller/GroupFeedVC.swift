d4fb736f8f840ce6d6e6eb2ce445c192a3741d80
Hope <hope@hopes-mac.fios-router.home>
2018-02-15 08:25:39 -0500

    Convert ids into email to display group members Initialized Group data for groups

--- breakpoint/Controller/GroupFeedVC.swift
+++ breakpoint/Controller/GroupFeedVC.swift
@@ -13,11 +13,27 @@ class GroupFeedVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var sendMessageView: UIView!
     @IBOutlet weak var messageTxtField: InsetTextField!
+    @IBOutlet weak var groupTitleLbl: UILabel!
+    @IBOutlet weak var sendBtn: UIButton!
+    @IBOutlet weak var membersLbl: UILabel!
     
+    var group: Group?
+    
+    func initData(forGroup group: Group)    {
+        self.group = group
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         sendMessageView.bindToKeyboard()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        groupTitleLbl.text = group?.groupTitle
+        DataService.instance.getEmailsFor(group: group!) { (returnedEmails) in
+            self.membersLbl.text = returnedEmails.joined(separator: ", ")
+        }
     }
 
    
